5ee5792a2524d374eae1baf1afe28866ddeff5e1
Ибрагимжонов Исломбек Илхомжон угли <unkiborg@gmail.com>
2022-04-21 12:30:31 +0500

    test04

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ global.app = {
     plugins: plugins,
 };
 
-//! This is version 03
+//! This is version 04
 // Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
