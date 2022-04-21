46b80bb9ea8dd725ef85d970e975ff10cbc06783
Ибрагимжонов Исломбек Илхомжон угли <unkiborg@gmail.com>
2022-04-21 11:39:44 +0500

    First commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,7 @@
+export let configFTP = {
+    host: "www.mygulp.com", // Адрес FTP сервера
+    port: 21,
+    user: "ftp_user", // Имя пользователя
+    password: "Daden123!", // Пароль
+    parallel: 5 // Количество одновременных потоков
+}
